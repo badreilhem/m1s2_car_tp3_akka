6d21059580c2c3170aa7f4e8f7927a4de4cfd61c
Badreddine Ilhem <badreddine@a10p24.fil.univ-lille1.fr>
2016-04-01 16:54:03 +0200

    le début

--- src/main/Node.java
+++ src/main/Node.java
@@ -1,5 +1,25 @@
 package main;
 
-public class Node {
+import akka.actor.UntypedActor;
+import akka.actor.ActorRef;
+import java.util.List;
+
+public class Node extends UntypedActor {
+	
+	List<ActorRef> childs;
+	
+	public Node(List<ActorRef> childs){
+		this.childs = childs;		
+	}
+
+	public void sendMessageToChilds(Object message){
+		for(ActorRef child : childs){
+			
+		}
+	}
+	
+	public void onReceive(Object arg0) throws Exception {
+			
+	}
 
 }
